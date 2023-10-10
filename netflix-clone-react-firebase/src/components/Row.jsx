484d1acfa1ba51e48b4db9f13fe2019750d70df5
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-10-10 16:55:08 +0300

    feat: react-netflix-clone finished frontend

---
+++ netflix-clone-react-firebase/src/components/Row.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+const Row = ({ title, fetchURL, rowID }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then(response => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+
+  const slideLeft = () => {
+    const slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    const slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+
+    if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+      slider.scrollLeft = 0;
+    }
+  };
+
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="bg-white left-5 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          fill="black"
+          size={40}
+        />
+        <div
+          id={"slider" + rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
+          {movies.map((item, id) => (
+            <Movie key={id} item={item} />
+          ))}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="bg-white right-5 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          fill="black"
+          size={40}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Row;
