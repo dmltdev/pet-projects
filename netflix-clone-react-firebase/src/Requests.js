b078e0e5347fe91aca2d9b609e8f0eee2785784c
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-10-11 10:26:02 +0300

    feat: adding firebase app

--- netflix-clone-react-firebase/src/Requests.js
+++ netflix-clone-react-firebase/src/Requests.js
@@ -1,5 +1,4 @@
-// TODO: Find a way to use .env file instead as a secure alternative
-const key = "0af6cf7e00c9b35e5ec5e35bd8329365";
+const key = process.env.TMDB_API_KEY;
 
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
